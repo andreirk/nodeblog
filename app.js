67319f092ac31cc77b5da38829569b5c6da20aa1
Andrey <sokolik_85@list.ru>
2016-07-03 10:50:11 +0000

    add New and create mehods of REST, add new form template and styles

--- app.js
+++ app.js
@@ -35,6 +35,21 @@ app.get('/blogs', function(req,res){
   
 });
 
+app.get('/blogs/new', function(req, res) {
+    res.render('new');
+})
+
+app.post('/blogs', function(req,res){
+    Blog.create(req.body.blog, function(err, NewBlog){
+       if(err){
+           res.render('new');
+           console.log(err);
+       }   else {
+           res.redirect('/blogs');
+       }
+    });
+  
+});
 
 
 
